561e4bd146dec3cede3e544877fb88109406304a
aceituna <juanhorcre@gmail.com>
2025-08-23 18:30:04 +0200

    Deps: added Qex to dep list

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule ExMq.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:qex, "~> 0.5"},
     ]
   end
 end
