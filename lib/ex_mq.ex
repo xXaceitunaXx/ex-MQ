d40aac8ff95013d99fa06fd6aa3ee9fc3a5b021a
aceituna <juanhorcre@gmail.com>
2025-08-23 18:31:47 +0200

    Update: Implemented core queue functionality to queue server process

--- lib/ex_mq.ex
+++ lib/ex_mq.ex
@@ -1,18 +1,31 @@
-defmodule ExMq do
-  @moduledoc """
-  Documentation for `ExMq`.
-  """
+defmodule Queue do
+  use GenServer
 
-  @doc """
-  Hello world.
+  @type queue :: list(String)
+  defstruct [:name, :queue]
 
-  ## Examples
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, name, name: String.to_atom(name))
+  end
 
-      iex> ExMq.hello()
-      :world
+  def init(name), do: {:ok, %{name: name, queue: Qex.new}}
 
-  """
-  def hello do
-    :world
+  def handle_call({:enqueue, message}, _from, state) do
+    n_q = Qex.push state.queue, message
+    {:reply, :ok, %{state | queue: n_q}}
+  end
+
+  def handle_call(:dequeue, _from, state) do
+    {result, n_q} = Qex.pop state.queue
+    {:reply, result, %{state | queue: n_q}}
+  end
+
+  def handle_call(:peek, _from, state) do
+    result = Qex.first state.queue
+    {:reply, result, state}
+  end
+
+  def handle_call(:name, _from, state) do # Necesario? veremos xd
+    {:reply, {:ok, state.name}, state}
   end
 end
