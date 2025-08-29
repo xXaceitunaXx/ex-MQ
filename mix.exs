e346f1766b9538edbc871b1b8b57112a16beae64
aceituna <juanhorcre@gmail.com>
2025-08-29 02:28:59 +0200

    Update: fixed to new version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMq.MixProject do
   def project do
     [
       app: :ex_mq,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
